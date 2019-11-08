27ed3e8d41873215f289ad1025d71c03ef9a00bd
matigumma <maalguna@gmail.com>
2019-11-08 17:54:46 -0300

    enviroment vars for mongo

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 //mongoose.connect(process.env.DBURI ||'mongodb+srv://mongo_freewaves:UsjQDN3WT7dpxSCZ@cluster0fw-alobx.mongodb.net/test?retryWrites=true&w=majority',{
-mongoose.connect('mongodb://mongo/controlpanel',{
+const URIS = 'mongodb://'+process.env.DB_MONGO_URI+'/'+process.env.DB_NAME;
+mongoose.connect(URIS,{
 autoReconnect: true,
     useCreateIndex: true,
     useNewUrlParser: true,
